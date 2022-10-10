0c9313f46d0e189b6d2d3323c3798e1e4352557c
Glaucoed <gesverzuthe@gmail.com>
2022-10-10 16:38:40 -0300

    realizado correções até o req05

---
+++ src/tests/helpers/mockData.js
@@ -0,0 +1,7 @@
+const mockData = {
+  response_code: 0,
+  response_message: 'Token Generated Successfully!',
+  token: 'ef9901aa5f17744396237b67c071caaa548e281e85780fd655d2488b3dc5e3d6',
+};
+
+export default mockData;
