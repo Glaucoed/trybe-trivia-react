c3c1ca61174d88fc14ca8071b4a3b6292a4916df
caiobacode <caioodsf@gmail.com>
2022-10-11 19:06:03 -0300

    requisito 10 e 11 pela metade

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,9 +1,16 @@
+import { SUBMIT_SCORE } from '../actions';
+
 const INITIAL_STATE = {
   score: 0,
 };
 
 function player(state = INITIAL_STATE, action) {
   switch (action.type) {
+  case SUBMIT_SCORE:
+    return {
+      ...state,
+      score: state.score + action.payload,
+    };
   default:
     return state;
   }
